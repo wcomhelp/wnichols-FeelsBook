47ba82e7cf264221322a0424ed7b5aee7fe5bafa
William Nichols <wnichols@cs.ualberta.ca>
2018-10-02 22:03:29 -0600

    Added emotion class & enabled onClick

---
+++ app/src/main/java/com/williamnichols/wnichols_feelsbook/Emotion.java
@@ -0,0 +1,43 @@
+package com.williamnichols.wnichols_feelsbook;
+
+import java.util.Date;
+
+/**
+ * Created by William Nichols on 10/2/18.
+ */
+
+public class Emotion {
+    /* Declaration of variables:
+     * emotionName:    the name of the emotion
+     * emotionDate:    the date the emotion took place
+     * emotionMessage: the optional message included with the emotion
+     */
+    private String emotionName;
+    private Date emotionDate;
+    private String emotionMessage;
+
+
+    public String getEmotionName() {
+        return emotionName;
+    }
+
+    public void setEmotionName(String emotionName) {
+        this.emotionName = emotionName;
+    }
+
+    public Date getEmotionDate() {
+        return emotionDate;
+    }
+
+    public void setEmotionDate(Date emotionDate) {
+        this.emotionDate = emotionDate;
+    }
+
+    public String getEmotionMessage() {
+        return emotionMessage;
+    }
+
+    public void setEmotionMessage(String emotionMessage) {
+        this.emotionMessage = emotionMessage;
+    }
+}
--- app/src/main/java/com/williamnichols/wnichols_feelsbook/MainActivity.java
+++ app/src/main/java/com/williamnichols/wnichols_feelsbook/MainActivity.java
@@ -1,13 +1,92 @@
 package com.williamnichols.wnichols_feelsbook;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FILENAME = "feelsbook.sav";
+
+    private ArrayList<Emotion> emotionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        saveInFile();
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+
+        loadFromFile();
+
+    }
+
+    private void loadFromFile() {
+        try {
+            FileInputStream fileInputStream = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fileInputStream));
+
+            Gson gson = new Gson();
+
+            Type listType = new TypeToken<ArrayList<Emotion>>(){}.getType();
+            emotionList = gson.fromJson(in, listType);
+
+        } catch (FileNotFoundException e) {
+            emotionList = new ArrayList<>();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private void saveInFile() {
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+
+            Gson gson = new Gson();
+            gson.toJson(emotionList, out);
+            out.flush();
+
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            // TODO: Handle the Exception properly later
+            throw new RuntimeException();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void handleEmotion(Button emotionButton) {
+        addRecord(emotionButton);
+    }
+
+    public void addRecord(Button emotionButton) {
+        Emotion emotion = new Emotion();
+        emotion.setEmotionName((String)emotionButton.getText());
+        emotion.setEmotionDate(new Date());
+        emotion.setEmotionMessage("Testing");
+        emotionList.add(emotion);
     }
 }
