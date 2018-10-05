00e70f4ed5812ee32a7aaff86a77052b1644c353
William Nichols <wnichols@cs.ualberta.ca>
2018-10-04 20:24:25 -0600

    Removed TODO stub, should have before.

--- app/src/main/java/com/williamnichols/wnichols_feelsbook/MainActivity.java
+++ app/src/main/java/com/williamnichols/wnichols_feelsbook/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
-        // TODO Auto-generated method stub
         super.onStart();
 
         loadFromFile();
