0195dc86a8260462ee43faae9dc55b366e82c800
William Nichols <wnichols@cs.ualberta.ca>
2018-10-04 20:24:46 -0600

    Spacing.

--- app/src/main/java/com/williamnichols/wnichols_feelsbook/MainActivity.java
+++ app/src/main/java/com/williamnichols/wnichols_feelsbook/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Emotion> emotionList;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
